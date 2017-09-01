ba77f1babf644ad786ab66f0428691ed2735ce78
xuzhenyang85 <moonniuniu@gmail.com>
2017-09-01 12:33:04 +0200

    new opdated

--- Exercise_EventsAndDOM2.js
+++ Exercise_EventsAndDOM2.js
@@ -3,22 +3,13 @@ var sayhi = function(id){
     document.getElementById("p1").innerHTML = "Hi from "+ id;
 }
 
-var arr = ["Lars","Peter","Jan","Ian"];
+var persons = ["Lars","Peter","Jan","Ian"];
+var personsList = persons.map(function(e){
+    return "<li>" + e + "</li>";
+});
+var newNames = personsList.join(" ");
+document.getElementById("ul").innerHTML = newNames;
 
-var addLi = function(arr){
-    var newarr =[];
-    for(var i = 0; i < arr.length;i++){
-        var ullarr = "<li>" + arr[i]+"</li>";
-        newarr.push(ullarr);
-    }
-    return newarr;
-    
-}
-var ekstra = "<ul>";
-var result = addLi(arr);
-var addUL = ekstra.concat(result).concat(ekstra);
-
-document.getElementById("ul").innerHTML = addUL;
 
 document.getElementById("name").addEventListener("click",function(event){
     event.preventDefault();
@@ -26,36 +17,22 @@ document.getElementById("name").addEventListener("click",function(event){
 
 var addname = function(){
     var input = document.getElementById("name").value;
-    arr.push(input);
-    arr.join("");
-    var newarr =[];
-    var addUl = function(arr){
-        for(var i = 0; i < arr.length;i++){
-            var ullarr = "<li>" + arr[i]+"</li>";
-            newarr.push(ullarr);
-        }
-        return newarr;
-    }
-    var ekstra = "<ul>";
-    var result = addUl(arr);
-    var result1 = ekstra.concat(result).concat(ekstra);
-    this.result1 = result1;
-    document.getElementById("ul").innerHTML =result1;
+    personsList.push("<li>"+input+"</li>");
+    var newNames = personsList.join(" ");
+    document.getElementById("ul").innerHTML = newNames;
+
 }
 
-var getCars= function(){
-    var cars = [
-        { id: 1, Year: 1997, Make: 'Ford', Model: 'E350', Price: 3000 },
-        { id: 2, Year: 1999, Make: 'Chevy', Model: 'Venture', Price: 4900 },
-        { id: 3, Year: 2000, Make: 'Chevy', Model: 'Venture', Price: 5000 },
-        { id: 4, Year: 1996, Make: 'Jeep', Model: 'Grand Cherokee', Price: 4799 },
-        { id: 5, Year: 2005, Make: 'Volvo', Model: 'V70', Price: 44799 }
-    ];
-    return cars;
-}
+var cars = [
+    { id: 1, Year: 1997, Make: 'Ford', Model: 'E350', Price: 3000 },
+    { id: 2, Year: 1999, Make: 'Chevy', Model: 'Venture', Price: 4900 },
+    { id: 3, Year: 2000, Make: 'Chevy', Model: 'Venture', Price: 5000 },
+    { id: 4, Year: 1996, Make: 'Jeep', Model: 'Grand Cherokee', Price: 4799 },
+    { id: 5, Year: 2005, Make: 'Volvo', Model: 'V70', Price: 44799 }
+];
+
 
 var addTabel = function (){
-    var cars = getCars();
     var thead = "<thead>";
     var theadEnd = "</thead>";
     var tr = "<tr>";
@@ -76,31 +53,22 @@ var addTabel = function (){
     document.getElementById("cars").innerHTML = start + carlist + end;
 }
 addTabel();
+document.getElementById("getPrice").addEventListener("click",function(e){
+    var inputPrice = document.getElementById("price").value;
+    var carsPrice = cars.filter(function (obj){
+        return obj.Price < inputPrice;
+    });
+    carsPrice.forEach(function(obj){
+        document.getElementById("cars2").innerHTML += "Id: " +obj.id + " Make: "+ obj.Make + " Model: " +obj.Model+ " Price: "+ obj.Price +"<br />";
+    });
+});
 
-var filterPrice = function (){
-    var price = document.getElementById("price").value;
-    var cars = getCars();
-    var thead = "<thead>";
-    var theadEnd = "</thead>";
-    var tr = "<tr>";
-    var trEnd = "</tr>";
-    var start = "<table class = \"table\">"+thead + tr + "<th>id</th><th>Year</th><th>Make</th><th>Model</th><th>Price</th>"+ trEnd + theadEnd;
-    
-    var result = cars.filter(function(obj,arg){
-        var filterlist = cars.Price < price;
-        return filterlist;
-    })
-    var end = "</table>";
-    document.getElementById("cars").innerHTML = result;
-}
-
-
-var clickMe = function (){
-    var num = document.getElementById("buttons").innerText;
-    var newNum = []
-    for(var i =0; i < num.length; i++ ){
-        return newNum.push(num[i]);
+document.getElementById("buttons").addEventListener("click",function(e){
+    var value = e.target.innerText;
+    if(value.innerText == "="){
+        document.getElementById("display").innerText +=100;
     }
-    document.getElementById("display").innerText = num;
-    alert(newNum);
-}
+    else{
+        document.getElementById("display").innerText +=value;
+    }
+});
